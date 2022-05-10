7055513bbb28311dd7331a1cbe86ed4360eb1a0b
JoelAbankwah <100015338+JoelAbankwah@users.noreply.github.com>
2022-05-09 18:56:39 -0700

    handle special requests for specific animals

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+      } else {
+        res.send(404);
+      }
+});
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -50,6 +59,11 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
   }
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+} 
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
