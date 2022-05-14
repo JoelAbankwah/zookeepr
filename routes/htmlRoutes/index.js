fc83fcbda2de17a0cca8c4fd5b2df34bc69b02a7
JoelAbankwah <100015338+JoelAbankwah@users.noreply.github.com>
2022-05-13 17:57:05 -0700

    modularized the code and added zookeepers

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const path = require('path');
+const animalRoutes = require('../apiRoutes/animalRoutes');
+const PORT = process.env.PORT || 3001;
+
+router.use(animalRoutes);
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+  
+router.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/animals.html'));
+});
+  
+router.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
+});
+  
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+  
+module.exports = router;
